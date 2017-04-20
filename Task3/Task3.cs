0d921613b0d35273184814d6dfeeeb32d79e75b3
nicknevolko <nikolay.nevolko@gmail.com>
2017-04-20 03:06:25 +0300

    added task 3

---
+++ Task3/Task3.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    public static class Task3
+    {
+        public static Dictionary<string, int> GetWords(string path)
+        {
+            if (ReferenceEquals(path, null)) throw new ArgumentNullException(nameof(path));
+            string text = System.IO.File.ReadAllText(path);
+            string[] words = text.Split(new Char[] { ' ', ',', '.', ':', '\t' });
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+
+            foreach (var s in words)
+            {
+                if (s.Trim() != "")
+                    if (!dict.ContainsKey(s))
+                        dict.Add(s, 1);
+                    else dict[s] += 1;
+            }
+
+            return dict;
+        }
+    }
+}
---
+++ TestsTask3/TestClass.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using static Task3.Task3;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestsTask3
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void TestTask3()
+        {
+            Dictionary<string, int> x = GetWords(@"file.txt");
+            Assert.AreEqual(x["ipsum"],3);
+            Assert.AreEqual(x["dolor"], 1);
+            Assert.AreEqual(x["lorem"], 1);
+            Assert.AreEqual(x["amet"], 2);
+            Assert.AreEqual(x["abcd"], 1);
+        }
+    }
+}
